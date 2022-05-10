6592b29a1bac9d941ffa49cf06c564bc2711df70
EmanuelPrima <eprima@frba.utn.edu.ar>
2022-05-10 10:21:28 -0300

    Versión final

--- tp1/main.c
+++ tp1/main.c
@@ -1,9 +1,36 @@
+// Emanuel Prima
+
+// gcc main.c -pedantic-errors -std=c11 -Wall -g -o prog
+
+#include <ctype.h>
 #include <stdio.h>
 
-int main() {
-    int a;
-    printf("Ingrese un numero:");
-    scanf("%d", &a);
-    printf("El doble de %d es %d", a, a*2);
-    return 0;
+int main(void) {
+  int lon = 20;
+  char cadena[lon];
+  int i = 0, j;
+  char c;
+
+  for (j = 0; j < lon; j++) {
+    cadena[j] = 0;
+  }
+
+  while (c != '\n') {
+    c = getchar();
+    cadena[i] = c;
+    i++;
+  }
+  cadena[i] = '\0';
+
+  for (j = 0; j < lon; j++) {
+    if (isupper(cadena[j]))
+      putchar(tolower(cadena[j]));
+    else if (islower(cadena[j]))
+      putchar(toupper(cadena[j]));
+    else if (isdigit(cadena[j]))
+      continue;
+    else
+      putchar(cadena[j]);
+  }
+  return 0;
 }
