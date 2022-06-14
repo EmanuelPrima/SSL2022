8ae1f22877369a64a40f46453f3a7b08099b5742
EmanuelPrima <eprima@frba.utn.edu.ar>
2022-06-14 12:09:04 -0300

    TP1 con cambios

--- tp1/main.c
+++ tp1/main.c
@@ -1,36 +1,27 @@
 // Emanuel Prima
 
+// Comentario: No había que copiar en un buffer (cadena) y la idea era cortar
+// por EOF, no por \n
+
 #include <ctype.h>
 #include <stdio.h>
 
 int main(void) {
-  int lon = 20;
-  char cadena[lon];
-  int i = 0, j;
   char c;
 
-  // Inicializo la cadena
-  for (j = 0; j < lon; j++) {
-    cadena[j] = 0;
-  }
-
   // Leo desde la consola
-  while (c != '\n') {
+  while (c != EOF) {
     c = getchar();
-    cadena[i] = c;
-    i++;
-  }
-  cadena[i] = '\0';
 
-  for (j = 0; j < lon; j++) {
-    if (isupper(cadena[j]))
-      putchar(tolower(cadena[j]));  // Si es una letra mayúscula, cambio a minúscula
-    else if (islower(cadena[j]))
-      putchar(toupper(cadena[j]));  // Si es una letra minúscula, cambio a mayúscula
-    else if (isdigit(cadena[j]))
+    if (isupper(c))
+      putchar(tolower(c));  // Si es una letra mayúscula, cambio a minúscula
+    else if (islower(c))
+      putchar(toupper(c));  // Si es una letra minúscula, cambio a mayúscula
+    else if (isdigit(c))
       continue;  // No copio
     else
-      putchar(cadena[j]);  // Todo lo demás se copia sin modificar
+      putchar(c);  // Todo lo demás se copia sin modificar
   }
+
   return 0;
 }
